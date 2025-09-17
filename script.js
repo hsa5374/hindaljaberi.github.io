94d97f05d831774e61abaecb8fecec122c053040
hsa5374 <hsa5374@nyu.edu>
2025-09-17 20:44:54 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple starter script
+console.log("Hello from script.js!");
+
+// Example of interactivity: alert when you click the page title
+document.getElementById("title")?.addEventListener("click", () => {
+  alert("You clicked the film title!");
+});
